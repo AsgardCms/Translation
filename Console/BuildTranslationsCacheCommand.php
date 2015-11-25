a896f721e99f8baf9214d02d130421d2fc4a7b42
Nicolas Widart <n.widart@gmail.com>
2015-11-25 20:29:34 +0100

    Create a console command to trigger the re-generation of translations cache

---
+++ Console/BuildTranslationsCacheCommand.php
@@ -0,0 +1,18 @@
+<?php namespace Modules\Translation\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Modules\Translation\Jobs\BuildTranslationsCache;
+
+class BuildTranslationsCacheCommand extends Command
+{
+    use DispatchesJobs;
+
+    protected $name = 'asgard:build:translations';
+    protected $description = 'Build the translations cache based on file and database';
+
+    public function fire()
+    {
+        $this->dispatch(new BuildTranslationsCache());
+    }
+}
