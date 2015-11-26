c492c9db49b96109dbe5de898864e283e8116fb5
Nicolas Widart <n.widart@gmail.com>
2015-11-26 21:15:10 +0100

    Adding config item

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
-    'name' => 'Translation'
+    /*
+    |--------------------------------------------------------------------------
+    | Allow static translations to be editable via GUI
+    |--------------------------------------------------------------------------
+    | Enabling this will have slight performance hit,
+    | about 20 to 50ms page load time increase.
+    */
+    'translations-gui' => true,
 ];
