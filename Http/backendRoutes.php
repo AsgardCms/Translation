0f4b39828e199a007650c0ebf12d17c8f08ee6ae
Nicolas Widart <n.widart@gmail.com>
2015-11-26 21:50:43 +0100

    Only keep required route

--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -5,17 +5,11 @@ use Illuminate\Routing\Router;
 /** @var Router $router */
 
 $router->group(['prefix' =>'/translation'], function (Router $router) {
-        $router->bind('translations', function ($id) {
-            return app('Modules\Translation\Repositories\TranslationRepository')->find($id);
-        });
-        $router->resource('translations', 'TranslationController', ['except' => ['show'], 'names' => [
-            'index' => 'admin.translation.translation.index',
-            'create' => 'admin.translation.translation.create',
-            'store' => 'admin.translation.translation.store',
-            'edit' => 'admin.translation.translation.edit',
-            'update' => 'admin.translation.translation.update',
-            'destroy' => 'admin.translation.translation.destroy',
-        ]]);
-// append
-
+    $router->bind('translations', function ($id) {
+        return app(\Modules\Translation\Repositories\TranslationRepository::class)->find($id);
+    });
+    get('translations', [
+        'uses' => 'TranslationController@index',
+        'as' => 'admin.translation.translation.index',
+    ]);
 });
