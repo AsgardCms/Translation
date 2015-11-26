1be4aaf47ac2c83145066b8e43adcc255576197a
Nicolas Widart <n.widart@gmail.com>
2015-11-26 21:50:30 +0100

    Remove unneded methods

--- Http/Controllers/Admin/TranslationController.php
+++ Http/Controllers/Admin/TranslationController.php
@@ -27,75 +27,8 @@ class TranslationController extends AdminBaseController
      */
     public function index()
     {
-        //$translations = $this->translation->all();
+        $translations = $this->translation->all();
 
-        return view('translation::admin.translations.index', compact(''));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        return view('translation::admin.translations.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        $this->translation->create($request->all());
-
-        flash()->success(trans('core::core.messages.resource created', ['name' => trans('translation::translations.title.translations')]));
-
-        return redirect()->route('admin.translation.translation.index');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  Translation $translation
-     * @return Response
-     */
-    public function edit(Translation $translation)
-    {
-        return view('translation::admin.translations.edit', compact('translation'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Translation $translation
-     * @param  Request $request
-     * @return Response
-     */
-    public function update(Translation $translation, Request $request)
-    {
-        $this->translation->update($translation, $request->all());
-
-        flash()->success(trans('core::core.messages.resource updated', ['name' => trans('translation::translations.title.translations')]));
-
-        return redirect()->route('admin.translation.translation.index');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  Translation $translation
-     * @return Response
-     */
-    public function destroy(Translation $translation)
-    {
-        $this->translation->destroy($translation);
-
-        flash()->success(trans('core::core.messages.resource deleted', ['name' => trans('translation::translations.title.translations')]));
-
-        return redirect()->route('admin.translation.translation.index');
+        return view('translation::admin.translations.index', compact('translations'));
     }
 }
