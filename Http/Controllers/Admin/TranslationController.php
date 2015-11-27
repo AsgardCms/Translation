e34a449887fb944c8ce5b09b356b49dd1b73d24f
Nicolas Widart <n.widart@gmail.com>
2015-11-27 15:00:15 +0100

    Adding assets specifically for this module only

--- Http/Controllers/Admin/TranslationController.php
+++ Http/Controllers/Admin/TranslationController.php
@@ -3,6 +3,7 @@
 use Modules\Core\Http\Controllers\Admin\AdminBaseController;
 use Modules\Translation\Repositories\TranslationRepository;
 use Modules\Translation\Services\TranslationsService;
+use Pingpong\Modules\Facades\Module;
 
 class TranslationController extends AdminBaseController
 {
@@ -21,8 +22,7 @@ class TranslationController extends AdminBaseController
 
         $this->translation = $translation;
         $this->translationsService = $translationsService;
-        $this->assetPipeline->requireJs('bootstrap-editable.js');
-        $this->assetPipeline->requireCss('bootstrap-editable.css');
+        $this->requireAssets();
     }
 
     /**
@@ -42,5 +42,13 @@ class TranslationController extends AdminBaseController
         }
 
         return view('translation::admin.translations.index', compact('translations'));
+    }
+
+    private function requireAssets()
+    {
+        $this->assetManager->addAsset('bootstrap-editable.css', Module::asset('translation:vendor/x-editable/dist/bootstrap3-editable/css/bootstrap-editable.css'));
+        $this->assetManager->addAsset('bootstrap-editable.js', Module::asset('translation:vendor/x-editable/dist/bootstrap3-editable/js/bootstrap-editable.min.js'));
+        $this->assetPipeline->requireJs('bootstrap-editable.js');
+        $this->assetPipeline->requireCss('bootstrap-editable.css');
     }
 }
