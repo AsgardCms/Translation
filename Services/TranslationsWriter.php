4c998527841ba2eaf16076802523186661501811
Nicolas Widart <n.widart@gmail.com>
2015-11-28 20:02:29 +0100

    Rename class

--- Services/TranslationsWriter.php
+++ Services/TranslationsWriter.php
@@ -3,7 +3,7 @@
 use Illuminate\Filesystem\Filesystem;
 use Modules\Translation\ValueObjects\TranslationGroup;
 
-class TranslationsExporter
+class TranslationsWriter
 {
     /**
      * @var Filesystem
