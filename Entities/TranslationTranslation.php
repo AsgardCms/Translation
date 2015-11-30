2fc264c0f999354d11096088fb3e603d51989cb8
Nicolas Widart <n.widart@gmail.com>
2015-11-30 14:59:17 +0100

    Adding the revisionable trait

--- Entities/TranslationTranslation.php
+++ Entities/TranslationTranslation.php
@@ -1,10 +1,21 @@
 <?php namespace Modules\Translation\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Venturecraft\Revisionable\RevisionableTrait;
 
 class TranslationTranslation extends Model
 {
+    use RevisionableTrait;
     public $timestamps = false;
     protected $fillable = ['value'];
     protected $table = 'translation__translation_translations';
+
+    protected $revisionEnabled = true;
+    protected $revisionCleanup = true;
+    protected $historyLimit = 100;
+
+    public static function boot()
+    {
+        parent::boot();
+    }
 }
