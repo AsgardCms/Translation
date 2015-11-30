850026aafd089b7d53c893700224dac4f59f4ee2
Selahattin Ünlü <selahattin.unlu@yandex.com>
2015-11-30 11:19:12 +0200

    add turkish language support to all modules

---
+++ Resources/lang/translation/tr/translations.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'title' => [
+        'translations' => 'Çeviriler',
+        'edit translation' => 'Çeviriyi düzenle',
+    ],
+    'button' => [
+        'create translation' => 'Yeni çeviri oluştur',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+];
