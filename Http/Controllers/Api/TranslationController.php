b773c0faa477f48be89e7fb9665fb42404e70285
Nicolas Widart <n.widart@gmail.com>
2015-11-30 20:26:41 +0100

    Adding route & method to get the translation history

--- Http/Controllers/Api/TranslationController.php
+++ Http/Controllers/Api/TranslationController.php
@@ -1,5 +1,6 @@
 <?php namespace Modules\Translation\Http\Controllers\Api;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\Translation\Repositories\TranslationRepository;
@@ -28,5 +29,37 @@ class TranslationController extends Controller
     public function clearCache()
     {
         $this->translation->clearCache();
+    }
+
+    public function revisions(Request $request)
+    {
+        $translation = $this->translation->findTranslationByKey($request->get('key'));
+        $translation = $translation->translate($request->get('locale'));
+
+        return response()->json($this->formatRevisionHistory($translation->revisionHistory));
+    }
+
+    private function formatRevisionHistory(Collection $revisionHistory)
+    {
+        $formattedHistory = [];
+
+        foreach ($revisionHistory as $history) {
+            $timeAgo = $history->created_at->diffForHumans();
+            if ($history->key == 'created_at' && !$history->old_value) {
+                $formattedHistory[] = trans('translation::translations.history created translation', [
+                    'name' => $history->userResponsible()->first_name,
+                    'time' => $timeAgo,
+                ]);
+            } else {
+                $formattedHistory[] = trans('translation::translations.history edited translation', [
+                    'name' => $history->userResponsible()->first_name,
+                    'oldValue' => $history->oldValue(),
+                    'newValue' => $history->newValue(),
+                    'time' => $timeAgo,
+                ]);
+            }
+        }
+
+        return $formattedHistory;
     }
 }
--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -3,9 +3,6 @@
 use Illuminate\Routing\Router;
 
 /** @var $router Router */
-post('translation/update', [
-    'uses' => 'TranslationController@update',
-    'as' => 'api.translation.translations.update'
-]);
-
+post('translation/update', ['uses' => 'TranslationController@update', 'as' => 'api.translation.translations.update', ]);
 post('translation/clearCache', ['uses' => 'TranslationController@clearCache', 'as' => 'api.translation.translations.clearCache']);
+post('translation/revisions', ['uses' => 'TranslationController@revisions', 'as' => 'api.translation.translations.revisions']);
