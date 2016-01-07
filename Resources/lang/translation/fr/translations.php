c8dc27d171e45da75fed1175732b7d4f860888a5
Cyril POIDEVIN <c.poidevin@axesys.fr>
2016-01-07 14:26:42 +0100

    add french translation for the module translation

---
+++ Resources/lang/translation/fr/translations.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'title' => [
+        'translations' => 'Traductions',
+        'edit translation' => '&Eacute;diter une traduction',
+    ],
+    'button' => [
+        'create translation' => 'Cr&eacute;er une traduction',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+    'Clear translation cache' => 'Nettoyer le cache des traductions',
+    'Export' => 'Exporter',
+    'Import' => 'Importer',
+    'Import csv translations file' => 'Importer un fichier de traductions',
+    'only for developers' => 'N&eacute;cessitera un gestion git !',
+    'Translations exported' => 'Traductions export&eacute;es.',
+    'Translations imported' => 'Traductions import&eacute;es.',
+    'select CSV file' => 'Selectionner un fichier CSV ...',
+    'csv only allowed' => 'Charger uniquement un fichier CSV, s\'il vous plait.',
+    'time' => 'Temps',
+    'author' => 'Auteur',
+    'history' => 'Historique',
+    'No Revisions yet' => 'Pas encore de r&eacute;visions',
+    'event' => '&Eacute;venement',
+    'created' => 'Cre&eacute;&eacute; le',
+    'edited' => '&Eacute;dit&eacute; le',
+];
