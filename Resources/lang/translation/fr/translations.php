9e552119d37e7806452032708fb57110514fbfe7
Josh Brown <joshua.brown@mccann.com>
2016-01-07 16:24:30 +0000

    Adding French translations

---
+++ Resources/lang/translation/fr/translations.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'title' => [
+        'translations' => 'Traductions',
+        'edit translation' => 'Éditer une traduction',
+    ],
+    'button' => [
+        'create translation' => 'Créer une traduction',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+    'Clear translation cache' => 'Nettoyer le cache des traductions',
+    'Export' => 'Exporter',
+    'Import' => 'Importer',
+    'Import csv translations file' => 'Importer un fichier de traductions',
+    'only for developers' => 'Seulement pour les développeurs',
+    'Translations exported' => 'Traductions exportées.',
+    'Translations imported' => 'Traductions importées.',
+    'select CSV file' => 'Selectionner un fichier CSV ...',
+    'csv only allowed' => 'Charger uniquement un fichier CSV, s\'il vous plait.',
+    'time' => 'Temps',
+    'author' => 'Auteur',
+    'history' => 'Historique',
+    'No Revisions yet' => 'Pas encore de révisions',
+    'event' => 'Évenement',
+    'created' => 'Créer le',
+    'edited' => 'Éditer le',
+];
