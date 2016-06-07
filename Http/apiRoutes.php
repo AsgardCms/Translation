b387a4d028e6c1ad09efbcc1a73d8688a0c613e0
Nicolas Widart <n.widart@gmail.com>
2016-06-07 21:29:43 +0200

    Using the $router variable instead of helpers

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Routing\Router;
-
 /** @var $router Router */
-post('translation/update', ['uses' => 'TranslationController@update', 'as' => 'api.translation.translations.update', ]);
-post('translation/clearCache', ['uses' => 'TranslationController@clearCache', 'as' => 'api.translation.translations.clearCache']);
-post('translation/revisions', ['uses' => 'TranslationController@revisions', 'as' => 'api.translation.translations.revisions']);
+
+$router->post('translation/update', ['uses' => 'TranslationController@update', 'as' => 'api.translation.translations.update', ]);
+$router->post('translation/clearCache', ['uses' => 'TranslationController@clearCache', 'as' => 'api.translation.translations.clearCache']);
+$router->post('translation/revisions', ['uses' => 'TranslationController@revisions', 'as' => 'api.translation.translations.revisions']);
--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -1,15 +1,14 @@
 <?php
 
 use Illuminate\Routing\Router;
-
 /** @var Router $router */
 
 $router->group(['prefix' =>'/translation'], function (Router $router) {
     $router->bind('translations', function ($id) {
         return \Modules\Translation\Entities\TranslationTranslation::find($id);
     });
-    get('translations', ['uses' => 'TranslationController@index', 'as' => 'admin.translation.translation.index', ]);
-    get('translations/update/{translations}', ['uses' => 'TranslationController@update', 'as' => 'admin.translation.translation.update', ]);
-    get('translations/export', ['uses' => 'TranslationController@export', 'as' => 'admin.translation.translation.export', ]);
-    post('translations/import', ['uses' => 'TranslationController@import', 'as' => 'admin.translation.translation.import', ]);
+    $router->get('translations', ['uses' => 'TranslationController@index', 'as' => 'admin.translation.translation.index', ]);
+    $router->get('translations/update/{translations}', ['uses' => 'TranslationController@update', 'as' => 'admin.translation.translation.update', ]);
+    $router->get('translations/export', ['uses' => 'TranslationController@export', 'as' => 'admin.translation.translation.export', ]);
+    $router->post('translations/import', ['uses' => 'TranslationController@import', 'as' => 'admin.translation.translation.import', ]);
 });
