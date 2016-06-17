42a945791ff6d66dc6d1312ce8242d68842c26b0
Nicolas Widart <n.widart@gmail.com>
2016-06-17 20:42:24 +0200

    Use new authorize middleware and cleanup permissions

--- Config/permissions.php
+++ Config/permissions.php
@@ -3,10 +3,8 @@
 return [
     'translation.translations' => [
         'index',
-        'update',
+        'edit',
         'export',
         'import',
     ],
-// append
-
 ];
--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -8,8 +8,24 @@ $router->group(['prefix' =>'/translation'], function (Router $router) {
     $router->bind('translations', function ($id) {
         return \Modules\Translation\Entities\TranslationTranslation::find($id);
     });
-    $router->get('translations', ['uses' => 'TranslationController@index', 'as' => 'admin.translation.translation.index', ]);
-    $router->get('translations/update/{translations}', ['uses' => 'TranslationController@update', 'as' => 'admin.translation.translation.update', ]);
-    $router->get('translations/export', ['uses' => 'TranslationController@export', 'as' => 'admin.translation.translation.export', ]);
-    $router->post('translations/import', ['uses' => 'TranslationController@import', 'as' => 'admin.translation.translation.import', ]);
+    $router->get('translations', [
+        'uses' => 'TranslationController@index',
+        'as' => 'admin.translation.translation.index',
+        'middleware' => 'can:translation.translations.index',
+    ]);
+    $router->get('translations/update/{translations}', [
+        'uses' => 'TranslationController@update',
+        'as' => 'admin.translation.translation.update',
+        'middleware' => 'can:translation.translations.edit',
+    ]);
+    $router->get('translations/export', [
+        'uses' => 'TranslationController@export', 
+        'as' => 'admin.translation.translation.export',
+        'middleware' => 'can:translation.translations.export',
+    ]);
+    $router->post('translations/import', [
+        'uses' => 'TranslationController@import', 
+        'as' => 'admin.translation.translation.import',
+        'middleware' => 'can:translation.translations.import',
+    ]);
 });
