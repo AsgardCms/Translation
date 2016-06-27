3bc9fa421e5cad69e72bef0e9228d47bd445068d
Nicolas Widart <n.widart@gmail.com>
2016-06-27 22:08:23 +0200

    Adding translation

--- Resources/lang/core/en/settings.php
+++ Resources/lang/core/en/settings.php
@@ -2,6 +2,7 @@
 
 return [
     'site-name' => 'Site name',
+    'site-name-mini' => 'Site name minimised',
     'site-description' => 'Site description',
     'template' => 'Front end template',
     'analytics-script' => 'Analytics Script',
