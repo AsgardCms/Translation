ba41fc6689a02f238f5bcb796bf308e5eb65e246
Jan Srb <jan.srb@gmail.com>
2016-11-21 22:08:14 +0100

    Translation of block, notification and page module

---
+++ Resources/lang/page/cs/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'page created' => 'Stránka vytvořena.',
+    'page not found' => 'Stránka nenalezena.',
+    'page updated' => 'Stránka upravena.',
+    'page deleted' => 'Stránka smazána.',
+
+    'template is required' => 'Název šablony je vyžadován.',
+    'title is required' => 'Název je vyžadován.',
+    'body is required' => 'Obsah je vyžadován.',
+    'only one homepage allowed' => 'Pouze jedna domovská stránka je povolena',
+];
